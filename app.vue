8d856bb795a78079b28795982b8047af818649cc
Hope Halfbreed <boldyrevanadezhda@gmail.com>
2025-02-18 02:36:55 +0700

    - Git init - App skeleton

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLoadingIndicator/>
+  <NuxtLayout>
+    <v-layout>
+      <AppHeader/>
+      
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        <NuxtPage/>
+      </v-main>
+      
+      <AppFooter/>
+    </v-layout>
+  </NuxtLayout>
+</template>
